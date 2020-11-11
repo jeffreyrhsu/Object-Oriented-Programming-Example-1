df2c7e8c496d69403fd04a2de66a02a95949d663
jeffreyrhsu <iamjeffreyhsu@gmail.com>
2020-11-11 20:55:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+// class Main {
+//   public static void main(String[] args) {
+//     System.out.println("Hello world!");
+//   }
+// }
+
+
+
+
+public class Person {
+  public void birthday() {
+    age++;
+  }
+  public int howOld() {
+
+  }
+  public static void main(String[] args){
+    Person bruno = new Person("Bruno");
+    Person beyonce = new Person("Beyonce");
+
+    if(beyonce.howOld() == 0)
+  }
+}
